266dfe6b3dbe5bd974580a0ee1c7c0fa2926b248
keylou <116100969+keylou@users.noreply.github.com>
2023-06-03 10:44:44 +0300

    Create Source3.cpp

---
+++ Source3.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+struct student //структура, в которой будет храниться информация о студенте (имя, фамилия, оценки)
+{
+    string name;
+    string surname;
+    string marks;
+};
+
+struct student_output //структура, в которой будет храниться информация о студенте (фамилия) и его средняя оценка
+{
+    string surname;
+    double average_mark = 0.0;
+};
+
+//функция для вычисления средней оценки
+double ReturnAvM(string& marks)
+{
+    double sum_marks = 0.0;
+    for (int i = 0; i < marks.size(); i++)
+    {
+        sum_marks += (marks[i] - '0');
+    }
+    return sum_marks / marks.size();
+}
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n; //кол-во студентов
+    int max_poor = 0; //максимальное количество неудовлетворительных оценок
+
+    //ввод данных
+    cout << "Введите количество студентов: "; cin >> n;
+    student* students = new student[n]; //входной массив структур
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Введите данные " << i + 1 << "-го студента (имя, фамилия, оценки подряд без пробелов): " << endl;
+        cin >> students[i].name;
+        cin >> students[i].surname;
+        cin >> students[i].marks;
+        int counter = 0;
+        for (int j = 0; j < students[i].marks.size(); j++)
+        {
+            if (students[i].marks[j] == '2')
+            {
+                counter++;
+            }
+        }
+        if (counter > max_poor)
+        {
+            max_poor = counter;
+        }
+    }
+
+    student_output* students_output = new student_output[n]; //выходной массив структур
+    //заполнение выходного массива в порядке, обратном заполнению входного массива
+    for (int i = 0, j = n - 1; i < n, j >= 0; i++, j--)
+    {
+        students_output[i].surname = students[j].surname;
+        students_output[i].average_mark = ReturnAvM(students[j].marks);
+    }
+    //вывод выходного массива
+    cout << endl << "Фамилии и средние баллы студентов: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << students_output[i].surname << " " << students_output[i].average_mark << endl;
+    }
+    //вывод дополнительного условия
+    cout << endl << "Студенты с наибольшим количеством неудовлетворительных оценок: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int counter = 0;
+        for (int j = 0; j < students[i].marks.size(); j++)
+        {
+            if (students[i].marks[j] == '2')
+            {
+                counter++;
+            }
+        }
+        if (counter == max_poor)
+        {
+            cout << students[i].surname << " " << students[i].marks << endl;
+        }
+    }
+}
